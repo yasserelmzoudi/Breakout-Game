dca6ab58bca31bd46f47164580897160675c6ad9
Jack Ellwood <jce22@duke.edu>
2020-09-14 21:32:27 -0400

    Block configuration set up and tests for blocks implemented

--- src/breakout/Game.java
+++ src/breakout/Game.java
@@ -1,5 +1,11 @@
 package breakout;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -22,6 +28,7 @@ public class Game extends Application {
   public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
   public static final Paint BACKGROUND = Color.AZURE;
   public static final Paint HIGHLIGHT = Color.OLIVEDRAB;
+  public static final String LEVEL = "testlevel.txt";
 
   private Scene myScene;
   private Paddle myPaddle;
@@ -33,7 +40,7 @@ public class Game extends Application {
    * @param stage the stage used to display the application
    */
   @Override
-  public void start(Stage stage) {
+  public void start(Stage stage) throws IOException, URISyntaxException {
     myScene = setupScene(SIZE, SIZE, BACKGROUND);
     stage.setScene(myScene);
     stage.setTitle(TITLE);
@@ -45,15 +52,44 @@ public class Game extends Application {
     animation.play();
   }
 
-  Scene setupScene(int width, int height, Paint background) {
+  Scene setupScene(int width, int height, Paint background) throws IOException, URISyntaxException {
     Group root = new Group();
     myPaddle = new Paddle();
     root.getChildren().add(myPaddle.getRectangle());
     myBall = new Ball();
     root.getChildren().add(myBall.getCircle());
+    buildBlocksFromFile(LEVEL, root);
     Scene scene = new Scene(root, width, height, background);
     scene.setOnKeyPressed(key -> handleKeyInput(key.getCode()));
     return scene;
+  }
+
+  /**
+   * Builds the blocks for a level from a text file
+   *
+   * @param level filename of the level
+   * @param root of the JavaFX resource tree
+   * @throws IOException
+   * @throws URISyntaxException
+   */
+  public void buildBlocksFromFile(String level, Group root) // Maybe this method should be in Block.java?
+      throws IOException, URISyntaxException {
+    Path path = Paths
+        .get(Objects.requireNonNull(Main.class.getClassLoader().getResource(level)).toURI());
+    int currentX;
+    int currentY = 0;
+
+    for (String row : Files.readAllLines(path)) {
+      currentX = 0;
+      for (String space : row.split("")) {
+        if (space.equalsIgnoreCase(Block.BLOCK_LETTER)) {
+          Block block = new Block(currentX, currentY);
+          root.getChildren().add(block.getRectangle());
+        }
+        currentX += Block.LENGTH;
+      }
+      currentY += Block.HEIGHT;
+    }
   }
 
   private void handleKeyInput(KeyCode code) {
