caf29a1c542af54e51c7c9377f709b037b9244cb
Yasser <ye9@duke.edu>
2020-09-27 18:09:19 -0400

    Made Balls added from MultiBallPowerUp have same velocity and position as MAIN_BALL

--- src/breakout/MultiBallPowerUp.java
+++ src/breakout/MultiBallPowerUp.java
@@ -13,8 +13,18 @@ public class MultiBallPowerUp extends PowerUp {
 
   @Override
   public void activate(Game game) {
+    double mainBallXPosition = game.getBall().getX();
+    double mainBallYPosition = game.getBall().getY();
+    double mainBallHorizontalSpeed = game.getBall().getHorizontalSpeed();
+    double mainBallVerticalSpeed = game.getBall().getVerticalSpeed();
+
     for (int i = 0; i < 2; i++) {
       Ball ball = new Ball();
+      ball.setX(mainBallXPosition);
+      ball.setY(mainBallYPosition);
+      ball.setHorizontalSpeed(mainBallHorizontalSpeed);
+      ball.setVerticalSpeed(mainBallVerticalSpeed);
+
       Platform.runLater(() -> game.getRoot().getChildren().add(ball.getCircle()));
       game.getBalls().add(ball);
       ball.setHorizontalSpeed(ThreadLocalRandom.current().nextDouble(0, Ball.HORIZONTAL_SPEED));
