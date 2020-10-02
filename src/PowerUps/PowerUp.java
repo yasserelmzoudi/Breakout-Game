ccf36d48f164a3a72c1b40cfc3218f9646b5092a
Yasser <ye9@duke.edu>
2020-10-02 03:16:17 -0400

    Added Javadoc comments to PowerUp

--- src/PowerUps/PowerUp.java
+++ src/PowerUps/PowerUp.java
@@ -7,6 +7,11 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+/**
+ * Abstraction representing the different type of PowerUps that are possible
+ *
+ * @author Yasser Elmzoud, Jack Ellwood
+ */
 public abstract class PowerUp {
 
   public static final double SPEED = 20;
@@ -19,6 +24,11 @@ public abstract class PowerUp {
   private final Text myText;
   private final Rectangle myRectangle;
 
+  /**
+   * Constructs a PowerUp at the given location
+   * @param x double representing x-coordinate of PowerUp
+   * @param y double representing y-coordinate of PowerUp
+   */
   public PowerUp(double x, double y) {
     myText = new Text(getImageString());
     myText.setX(x);
@@ -31,6 +41,12 @@ public abstract class PowerUp {
     myRectangle.setId("powerUpRectangle" + x + y);
   }
 
+  /**
+   * Generates a random PowerUp from possible PowerUps
+   * @param x double representing x-coordinate of PowerUp
+   * @param y double representing y-coordinate of PowerUp
+   * @return PowerUp representing randomly generated PowerUp
+   */
   public static PowerUp powerUpGenerator(double x, double y) {
     PowerUp newPowerUp;
     int randomPowerUpType = ThreadLocalRandom.current().nextInt(0, 3);
@@ -42,6 +58,12 @@ public abstract class PowerUp {
     return newPowerUp;
   }
 
+  /**
+   * Causes a PowerUp to fall from a destroyed Brick
+   * @param game Game in which the PowerUp falls
+   * @param elapsedTime double representing time that has passed
+   * @return boolean representing if PowerUp collided with a Paddle
+   */
   public boolean fallFromDestroyedBlock(Game game, double elapsedTime) {
     myText.setY(myText.getY() + elapsedTime * SPEED);
     myRectangle.setY(myRectangle.getY() + elapsedTime * SPEED);
@@ -54,16 +76,34 @@ public abstract class PowerUp {
     return false;
   }
 
+  /**
+   * Checks to see if PowerUp has collided with Paddle
+   * @param paddle Paddle to be checked for collision with PowerUp
+   * @return boolean representing if Paddle has collided with PowerUp
+   */
   public boolean collisionWithPaddle(Paddle paddle) {
     return getBottom() >= Paddle.STARTING_Y && getTop() <= Paddle.STARTING_Y + Paddle.HEIGHT
         && getRightSideX() >= paddle.getLeftX()
         && getLeftSideX() <= paddle.getRightX();
   }
 
+  /**
+   * Activates the ability of the PowerUp and applies it to the given Game
+   * @param game Game that is to be adjusted based on the PowerUp's abilities
+   */
   public abstract void activate(Game game);
 
+  /**
+   * Deactivates the ability of the PowerUp and removes it from the given Game
+   * @param game Game that is to be adjusted based on the PowerUp's abilities
+   * @return boolean representing if PowerUp has been deactivated
+   */
   public abstract boolean deactivate(Game game);
 
+  /**
+   * Returns String representing name of PowerUp
+   * @return String representing name of PowerUp
+   */
   public abstract String getImageString();
 
   private double getLeftSideX() {
